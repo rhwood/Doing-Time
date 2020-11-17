b6d6bd005e3400cd4c71d6a4bce3b3e547f5bb23
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-11-17 05:53:38 -0500

    fix: remove AppStore handling

--- Classes/Doing_TimeAppDelegate.h
+++ Classes/Doing_TimeAppDelegate.h
@@ -20,7 +20,6 @@
 
 #import <UIKit/UIKit.h>
 //#import <EventKit/EventKit.h>
-#import "AppStoreDelegate.h"
 #import "Constants.h"
 
 @class MainViewController;
@@ -28,15 +27,11 @@
 @interface Doing_TimeAppDelegate : NSObject <UIApplicationDelegate> {
     UIWindow *window;
     MainViewController *mainViewController;
-	AppStoreDelegate *_appStore;
-//	EKEventStore *_eventStore;
 }
 
 @property (nonatomic, strong) IBOutlet UIWindow *window;
 @property (nonatomic, strong) IBOutlet MainViewController *mainViewController;
-//@property (nonatomic, strong) IBOutlet EKEventStore *eventStore;
 
-@property (nonatomic, strong) IBOutlet AppStoreDelegate *appStore;
 @property (readonly) UIColor *red;
 @property (readonly) UIColor *blue;
 @property (readonly) UIColor *green;
--- Classes/SettingsViewController.m
+++ Classes/SettingsViewController.m
@@ -51,9 +51,6 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    NSLog(@"%lu IAP products available", (unsigned long)self.appDelegate.appStore.validProducts.count);
-    NSLog(@"%@ to make purchases", self.appDelegate.appStore.canMakePayments ? @"Allowed" : @"Not allowed");
-    NSLog(@"%@ transactions for all products", [self.appDelegate.appStore hasTransactionsForAllProducts] ? @"Has" : @"Does not have");
 }
 
 - (void)didReceiveMemoryWarning {
@@ -74,13 +71,6 @@
 		section++;
 	}
 	switch (section) {
-		case IAPSection:
-            if (self.appDelegate.appStore.validProducts.count > 0) {
-                return self.appDelegate.appStore.validProducts.count + 1;
-            } else {
-                return 0;
-            }
-			break;
 		case SupportSection:
 			return 2;
 			break;
@@ -108,24 +98,6 @@
     cell.accessoryView = nil;
 	
     switch (section) {
-		case IAPSection:
-			if (self.appDelegate.appStore.canMakePayments) {
-                if (indexPath.row < self.appDelegate.appStore.validProducts.count) {
-                    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-                    SKProduct *product = [self.appDelegate.appStore.products objectForKey:[self.appDelegate.appStore.validProducts objectAtIndex:indexPath.row]];
-                    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
-                    [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
-                    [numberFormatter setLocale:product.priceLocale];
-                    cell.textLabel.text = product.localizedTitle;
-                    cell.detailTextLabel.text = [NSString localizedStringWithFormat:NSLocalizedString(@"%@ for %@", @"String containing the description of an in-app purchase followed by the cost."),
-                                                 product.localizedDescription,
-                                                 [numberFormatter stringFromNumber:product.price]];
-                } else {
-                    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-                    cell.textLabel.text = NSLocalizedString(@"Restore Purchases", @"Label for link to restore purchases");
-                }
-			}
-			break;
 		case SupportSection:
 			cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 			switch (indexPath.row) {
@@ -166,19 +138,6 @@
 		section++;
 	}
 	switch (section) {
-		case IAPSection:
-            if (!self.appDelegate.appStore.canMakePayments) {
-                return [NSString localizedStringWithFormat:NSLocalizedString(@"In-App Purchases are disabled on this %@.", @"Notice that the user cannot purchase an available upgrade due to policy."),
-                        [UIDevice currentDevice].localizedModel];
-            } else if (![self.appDelegate.appStore hasTransactionsForAllProducts] &&
-                       ![self.appDelegate.appStore hasDataForAnyProducts]) {
-                if ([self.appDelegate.appStore.openRequests count]) {
-                    return NSLocalizedString(@"Getting available upgrades...", @"Notice that the application is getting the list of available in-app purchases.");
-                } else {
-                    return NSLocalizedString(@"Unable to get available upgrades.", @"Notice that the application cannot get the list of available in-app purchases.");
-                }
-            }
-			break;
 		case SupportSection:
             return [NSString localizedStringWithFormat:NSLocalizedString(@"%@ version %@ (%@)", @"About view version footer"),
                     [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"],
@@ -214,21 +173,6 @@
 	self.showErrorAlert = YES;
 	[self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionNone animated:YES];
 	switch (section) {
-		case IAPSection: // Store
-			if (self.appDelegate.appStore.canMakePayments) {
-                if (indexPath.row < self.appDelegate.appStore.products.count) {
-                    SKProduct *product = [self.appDelegate.appStore.products objectForKey:[[self.appDelegate.appStore.products allKeys] objectAtIndex:indexPath.row]];
-                    //self.activityLabel.text = [NSString localizedStringWithFormat:NSLocalizedString(@"Getting %@...", @"Label indicating that app is getting an in-app purchase (with %@ as the title)"), product.localizedTitle];
-                    //[self hidePurchaseActivity:NO];
-                    [self.appDelegate.appStore queuePaymentForProduct:product];
-                } else {
-                    //TODO: add an activity indicator
-                    //self.activityLabel.text = NSLocalizedString(@"Restoring purchases...", @"Label indicating that app is restoring purchases");
-                    //[self hidePurchaseActivity:NO];
-                    [self.appDelegate.appStore restoreCompletedTransactions];
-                }
-			}
-			break;
 		case SupportSection: // About
 			switch (indexPath.row) {
 				case 0:
